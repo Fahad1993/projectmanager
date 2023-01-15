5a93f3a31877957110b8d525be2707435f820b47
Fahad1993 <foso1015@gmail.com>
2023-01-15 18:15:48 +0300

    handle with a Delete projects and todos

--- pages/api/project.js
+++ pages/api/project.js
@@ -2,29 +2,41 @@ import prisma from 'lib/prisma'
 import { getSession } from 'next-auth/react'
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(501).end()
-  }
+    if (req.method !== 'POST' && req.method !== 'DELETE') {
+        return res.status(501).end()
+    }
 
-  const session = await getSession({ req })
-  if (!session) return res.status(401).json({ message: 'Not logged in' })
+    const session = await getSession({ req })
+    if (!session) return res.status(401).json({ message: 'Not logged in' })
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id: session.user.id,
-    },
-  })
+    const user = await prisma.user.findUnique({
+        where: {
+            id: session.user.id,
+        },
+    })
 
-  if (!user) return res.status(401).json({ message: 'User not found' })
+    if (!user) return res.status(401).json({ message: 'User not found' })
+    if (req.method === 'POST') {
+        await prisma.project.create({
+            data: {
+                name: req.body.name,
+                owner: {
+                    connect: { id: user.id },
+                },
+            },
+        })
+    }
+    // handle this with a DELETE API call
+    if (req.method === 'DELETE') {
+        await prisma.project.deleteMany({
+            where: {
+                id: req.body.id,
+                owner: {
+                    id: user.id,
+                },
+            },
+        })
+    }
 
-  await prisma.project.create({
-    data: {
-      name: req.body.name,
-      owner: {
-        connect: { id: user.id },
-      },
-    },
-  })
-
-  res.end()
+    res.end()
 }
--- pages/api/todo.js
+++ pages/api/todo.js
@@ -2,29 +2,43 @@ import prisma from 'lib/prisma'
 import { getSession } from 'next-auth/react'
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(501).end()
-  }
+    if (req.method !== 'POST' && req.method !== 'DELETE') {
+        return res.status(501).end()
+    }
 
-  const session = await getSession({ req })
-  if (!session) return res.status(401).json({ message: 'Not logged in' })
+    const session = await getSession({ req })
+    if (!session) return res.status(401).json({ message: 'Not logged in' })
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id: session.user.id,
-    },
-  })
+    const user = await prisma.user.findUnique({
+        where: {
+            id: session.user.id,
+        },
+    })
 
-  if (!user) return res.status(401).json({ message: 'User not found' })
+    if (!user) return res.status(401).json({ message: 'User not found' })
 
-  await prisma.todo.create({
-    data: {
-      name: req.body.name,
-      project: {
-        connect: { id: req.body.project_id },
-      },
-    },
-  })
-
-  res.end()
+    if (req.method === 'POST') {
+        await prisma.todo.create({
+            data: {
+                name: req.body.name,
+                project: {
+                    connect: { id: req.body.project_id },
+                },
+            },
+        })
+    }
+    //handle this with a DELETE API call
+    if (req.method === 'DELETE') {
+        await prisma.todo.deleteMany({
+            where: {
+                id: req.body.id,
+                project: {
+                    owner: {
+                        id: user.id,
+                    },
+                },
+            },
+        })
+    }
+    res.end()
 }
